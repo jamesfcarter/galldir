39a5772f23daebe598fe0a7f83960630e7396643
James F. Carter <james@jfc.org.uk>
2019-03-26 11:00:46 +0000

    TestImageContent

--- provider_test.go
+++ provider_test.go
@@ -1,6 +1,9 @@
 package galldir_test
 
 import (
+	"crypto/sha1"
+	"fmt"
+	"io"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -61,6 +64,44 @@ func TestAlbum(t *testing.T) {
 			}
 			if tc.name != "" && album.Name != tc.name {
 				t.Errorf("unexpected album name: %s", album.Name)
+			}
+		})
+	}
+}
+
+func TestImageContent(t *testing.T) {
+	tests := []struct {
+		path      string
+		hash      string
+		expectErr bool
+	}{
+		{
+			path:      "/not_there.jpg",
+			expectErr: true,
+		},
+		{
+			path: "/subalbum/icon.png",
+			hash: "aa72605dbcb4f8b933be68f0d11391673cd9ecc7",
+		},
+	}
+	provider := galldir.Provider{galldir.FsBackend("testdata/album")}
+	for _, tc := range tests {
+		t.Run(tc.path, func(t *testing.T) {
+			r, err := provider.ImageContent(tc.path)
+			if err == nil && tc.expectErr {
+				t.Fatal("expected an error")
+			}
+			if err != nil && !tc.expectErr {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if err != nil || tc.hash == "" {
+				return
+			}
+			hasher := sha1.New()
+			io.Copy(hasher, r)
+			sha := fmt.Sprintf("%x", hasher.Sum(nil))
+			if sha != tc.hash {
+				t.Errorf("bad hash: %s", sha)
 			}
 		})
 	}
